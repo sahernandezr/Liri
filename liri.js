8347519fd8e9d6f7f0e1d2cae2f494fb1e764d1e
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-11-10 11:21:03 -0600

    Spotify is working

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
+var moment = require("moment");
 
 var action = process.argv[2];
 console.log(action);
@@ -30,9 +31,8 @@ function concert() {
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(
         function (response) {
-            // console.log(artist.toUpperCase()
             for (var i = 0; i < response.data.length; i++) {
-                console.log("Venue: "+JSON.stringify(response.data[i].venue.name)+"\nLocation: "+response.data[i].venue.city+", "+response.data[i].venue.country+"\nDate: "+response.data[i].datetime+"\n--------");
+                console.log("Venue: " + JSON.stringify(response.data[i].venue.name) + "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country + "\nDate: " + moment(response.data[i].datetime).format('L') + "\n--------");
             };
         }
 
@@ -55,8 +55,23 @@ function concert() {
 }
 
 function song() {
-    console.log("Great song");
-};
+    if (process.argv.length === 3) {
+        var querysongURL = "https://api.spotify.com/v1/search?query=The+Sign+ace+of+base&type=track&offset=0&limit=1";
+    }
+    else {
+        var track = process.argv.slice(3).join('+');
+        var querysongURL = "https://api.spotify.com/v1/search?query=" + track + "&type=track&offset=0&limit=1";
+    }
+
+    spotify
+            .request(querysongURL)
+            .then(function (response) {
+                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+            });
+}
 
 function movie() {
     console.log("Great movie!");
