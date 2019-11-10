a9d18ba6e7a3dd764310ec0e4a0e416e4b7a2d51
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-11-10 17:44:25 -0600

    random spotify-this working

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
+var fs = require("fs");
 
 var action = process.argv[2];
 
@@ -26,9 +27,9 @@ switch (action) {
 }
 
 function concert() {
-    
-    var artist = process.argv.slice(3).join('');
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    var searchTerm = process.argv.slice(3).join('');
+    var queryURL = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(
         function (response) {
             for (var i = 0; i < response.data.length; i++) {
@@ -54,52 +55,78 @@ function concert() {
         });
 }
 
-function song() {
-   
+function song(searchTerm) {
+
     if (process.argv.length === 3) {
-        var querysongURL = "https://api.spotify.com/v1/search?query=The+Sign+ace+of+base&type=track&offset=0&limit=1";
+        var queryURL = "https://api.spotify.com/v1/search?query=The+Sign+ace+of+base&type=track&offset=0&limit=1";
     }
     else {
-        var track = process.argv.slice(3).join('+');
-        var querysongURL = "https://api.spotify.com/v1/search?query=" + track + "&type=track&offset=0&limit=1";
+        var searchTerm = process.argv.slice(3).join('+');
+        var queryURL = "https://api.spotify.com/v1/search?query=" + searchTerm + "&type=track&offset=0&limit=1";
     }
 
     spotify
-            .request(querysongURL)
-            .then(function (response) {
-                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
-            })
-            .catch(function (err) {
-                console.error('Error occurred: ' + err);
-            });
+        .request(queryURL)
+        .then(function (response) {
+            console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
+        })
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
 }
 
 function movie() {
-    
+
     if (process.argv.length === 3) {
-        var querymovieURL = "https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+        var queryURL = "https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
         //console.log(querymovieURL);
     }
     else {
-        var movie = process.argv.slice(3).join('+');
-        var querymovieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        var searchTerm = process.argv.slice(3).join('+');
+        var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
         //console.log(querymovieURL);
     }
 
-    axios.get(querymovieURL).then(
+    axios.get(queryURL).then(
         function (res) {
             //console.log(res);
 
-            if (res.data.Ratings.length>1) {
-            console.log("\nTitle: "+res.data.Title+"\nYear: "+res.data.Year+"\nIMDB Rating: "+res.data.imdbRating+"/10\nRotten Tomatoes Rating: "+res.data.Ratings[1].Value+"\nCountry: "+res.data.Country+"\nLanguage: "+res.data.Language+"\nPlot: "+res.data.Plot+"\nActors: "+res.data.Actors+"\n------\n");
-        }
+            if (res.data.Ratings.length > 1) {
+                console.log("\nTitle: " + res.data.Title + "\nYear: " + res.data.Year + "\nIMDB Rating: " + res.data.imdbRating + "/10\nRotten Tomatoes Rating: " + res.data.Ratings[1].Value + "\nCountry: " + res.data.Country + "\nLanguage: " + res.data.Language + "\nPlot: " + res.data.Plot + "\nActors: " + res.data.Actors + "\n------\n");
+            }
             else {
-                console.log("\nTitle: "+res.data.Title+"\nYear: "+res.data.Year+"\nIMDB Rating: "+res.data.imdbRating+"\nRotten Tomatoes Rating: "+"Not available"+"\nCountry: "+res.data.Country+"\nLanguage: "+res.data.Language+"\nPlot: "+res.data.Plot+"\nActors: "+res.data.Actors+"\n------\n");
+                console.log("\nTitle: " + res.data.Title + "\nYear: " + res.data.Year + "\nIMDB Rating: " + res.data.imdbRating + "\nRotten Tomatoes Rating: " + "Not available" + "\nCountry: " + res.data.Country + "\nLanguage: " + res.data.Language + "\nPlot: " + res.data.Plot + "\nActors: " + res.data.Actors + "\n------\n");
             }
         }
     )
 };
 
 function dowhatisay() {
-    console.log("I will follow the instructions!");
-}
+    //console.log("I will follow the instructions!");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        else {
+            var dataArray = data.split(",");
+            action = dataArray[0];
+            if (action === "spotify-this-song") {
+                searchTerm = dataArray[1].split(' ').join('+');
+            }
+            var queryURL = "https://api.spotify.com/v1/search?query=" + searchTerm + "&type=track&offset=0&limit=1";
+        }
+
+        spotify
+            .request(queryURL)
+            .then(function (response) {
+                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+            });
+
+
+
+    })
+
+    }
