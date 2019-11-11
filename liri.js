43e31397fd2e1f4d8f7b3d403a0f7d9168d453e1
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-11-10 18:07:04 -0600

    random concert this is working

--- liri.js
+++ liri.js
@@ -55,8 +55,7 @@ function concert() {
         });
 }
 
-function song(searchTerm) {
-
+function song() {
     if (process.argv.length === 3) {
         var queryURL = "https://api.spotify.com/v1/search?query=The+Sign+ace+of+base&type=track&offset=0&limit=1";
     }
@@ -79,12 +78,12 @@ function movie() {
 
     if (process.argv.length === 3) {
         var queryURL = "https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
-        //console.log(querymovieURL);
+        
     }
     else {
         var searchTerm = process.argv.slice(3).join('+');
         var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
-        //console.log(querymovieURL);
+        
     }
 
     axios.get(queryURL).then(
@@ -104,29 +103,61 @@ function movie() {
 function dowhatisay() {
     //console.log("I will follow the instructions!");
     fs.readFile("random.txt", "utf8", function (error, data) {
+        var dataArray = data.split(",");
+        var action = dataArray[0]
+
         if (error) {
             return console.log(error);
         }
-        else {
-            var dataArray = data.split(",");
-            action = dataArray[0];
-            if (action === "spotify-this-song") {
-                searchTerm = dataArray[1].split(' ').join('+');
-            }
+
+        if (action === "spotify-this-song") {
+            searchTerm = dataArray[1].split(' ').join('+');
+
             var queryURL = "https://api.spotify.com/v1/search?query=" + searchTerm + "&type=track&offset=0&limit=1";
+
+            spotify
+                .request(queryURL)
+                .then(function (response) {
+                    console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
+                })
+                .catch(function (err) {
+                    console.error('Error occurred: ' + err);
+                });
         }
 
-        spotify
-            .request(queryURL)
-            .then(function (response) {
-                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name + "\nSong's name: " + response.tracks.items[0].name + "\nSpotify's preview link: " + response.tracks.items[0].external_urls.spotify + "\nAlbum's name:  " + response.tracks.items[0].album.name + "\n------\n");
-            })
-            .catch(function (err) {
-                console.error('Error occurred: ' + err);
-            });
+        if (action==="concert-this") {
+            searchTerm = dataArray[1].split(' ').join('');
+            var queryURL = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
+            console.log("\nArtist: "+dataArray[1]+"\n");
+            axios.get(queryURL).then(
+                function (response) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        console.log("Venue: " + JSON.stringify(response.data[i].venue.name) + "\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country + "\nDate: " + moment(response.data[i].datetime).format('L') + "\n--------");
+                    };
+                }
+        
+            )
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                });
 
+        }
 
+        if (action==="movie-this") {
 
+        }
     })
 
-    }
+}
